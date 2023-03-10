c89b0db4cb8ec125040e7e1e66b3880598c39eb5
Frédéric Blanqui <frederic.blanqui@inria.fr>
2023-03-10 19:10:21 +0100

    modify parsing of arguments

--- main.ml
+++ main.ml
@@ -10,9 +10,16 @@ let usage() =
     Sys.argv.(0)
 
 let wrong_arg_nb() =
-  Printf.eprintf "wrong number of arguments\n%!";
-  usage();
-  exit 1
+  Printf.eprintf "wrong number of arguments\n%!"; usage(); exit 1
+
+(* [split x l] returns a pair of lists [l1,l2] such that [l = rev l1 @
+   x :: l2], and raises [Not_found] if [x] does not occur in [l]. *)
+let split x =
+  let rec aux acc l =
+    match l with
+    | [] -> raise Not_found
+    | y::m -> if y = x then acc, m else aux (y::acc) m
+  in aux []
 
 let main() =
 
@@ -21,10 +28,15 @@ let main() =
   if n < 1 || n > 4 then wrong_arg_nb();
 
   (* parse arguments *)
-  let stats, filename, (*other*) args =
-    if Sys.argv.(1) = "--stats" then
-      if n <> 2 then wrong_arg_nb() else true, Sys.argv.(2), [||]
-    else false, Sys.argv.(1), Array.sub Sys.argv 2 (n-1)
+  let args = List.tl (Array.to_list Sys.argv) in
+  let stats, args =
+    try let l1, l2 = split "--stats" args in true, List.rev_append l1 l2
+    with Not_found -> false, args
+  in
+  let filename, args =
+    match args with
+    | [] -> wrong_arg_nb()
+    | filename :: args -> filename, args
   in
   let dk =
     match Filename.extension filename with
@@ -34,10 +46,10 @@ let main() =
   in
   let basename = Filename.chop_extension filename in
   let range =
-    match Array.length args with
-    | 0 -> All
-    | 1 -> Only (int_of_string args.(0))
-    | 2 -> Inter (int_of_string args.(0), int_of_string args.(1))
+    match args with
+    | [] -> All
+    | [x] -> Only (int_of_string x)
+    | [x;y] -> Inter (int_of_string x, int_of_string y)
     | _ -> wrong_arg_nb()
   in
 
@@ -51,14 +63,15 @@ let main() =
   the_definitions := input_value ic;
   let nb_proofs = input_value ic in
   log "%d proof steps\n%!" nb_proofs;
-  update_map_const_typ_vars_pos();
+
+  (* prepare proof reading *)
+  let dump_file = basename ^ ".prf" in
+  log "read %s ...\n%!" dump_file;
+  init_proof_reading dump_file nb_proofs;
 
   (* print stats *)
   if stats then
     begin
-      let dump_file = basename ^ ".prf" in
-      set_dump_file dump_file nb_proofs;
-      log "read %s ...\n%!" dump_file;
       iter_proofs (fun _ p -> count_thm_uses p; count_rule_uses p);
       log "compute statistics ...\n";
       print_thm_uses_histogram();
@@ -66,29 +79,22 @@ let main() =
       exit 0
     end;
 
-  (* generate signature related files *)
-  if not dk then
-    begin
-      Xlp.export_types basename;
-      Xlp.export_terms basename;
-      Xlp.export_axioms basename
-    end;
-
-  (* read proofs before range start *)
-  let dump_file = basename ^ ".prf" in
-  set_dump_file dump_file nb_proofs;
-  log "read %s ...\n%!" dump_file;
+  (* read proofs up to range start *)
   begin match range with
   | Only x | Inter(x,_) -> for k = 0 to x-1 do ignore (proof_at k) done
+  | All -> ()
   | Upto _ -> assert false
-  | All -> iter_proofs (fun _ _ -> ())
   end;
 
   (* generate output *)
+  update_map_const_typ_vars_pos();
   if dk then Xdk.export_to_dk_file basename range
   else
     begin
-      Xlp.export_proofs basename range;
+      Xlp.export_types basename;
+      Xlp.export_terms basename;
+      Xlp.export_axioms basename;
+      Xlp.export_proofs basename range
       Xlp.export_term_abbrevs basename;
       Xlp.export_type_abbrevs basename;
     end
