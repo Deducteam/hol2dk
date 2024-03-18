7de98bca03058145fd68242168d0d5b451c189a4
Frédéric Blanqui <fblanqui@users.noreply.github.com>
2024-03-18 13:30:04 +0100

    compute size by using position (#107)

--- xlib.ml
+++ xlib.ml
@@ -608,28 +608,24 @@ let canonical_term =
    canonical type variables [a0, a1, ...], [vs] are replaced by
    canonical term variables [x0, x1, ...], and the abstracted term
    variables are replaced by canonical variables [y0, y1, ...]. Hence,
-   if [t'] is alpha-equivalent to [t], then [canonical_term t' = u],
-- [n] is the size of [t]. *)
+   if [t'] is alpha-equivalent to [t], then [canonical_term t' = u]. *)
 let canonical_term
-    : term -> hol_type list * term list * hol_type list * term * int =
+    : term -> hol_type list * term list * hol_type list * term =
   (*let a_max = ref 0 and x_max = ref 0 and y_max = ref 0 in*)
   let sy = Array.init 50 (fun i -> "y" ^ string_of_int i) in
   (* [subst i su t] applies [su] on [t] and rename abstracted
      variables as well by incrementing the integer [i]. [su] is a term
      substitution mapping term variables abstracted in [t] by the
      canonical term variables [y0, y1, ...]. *)
-  let size = ref 0 in
-  let rec subst i su t = incr size;
+  let rec subst i su t =
     (*log "subst %d %a %a\n%!" i (olist (opair oterm oterm)) su oterm t;*)
     match t with
     | Var _ -> (try List.assoc t su with Not_found -> assert false)
-    | Const(s,b) ->
-       size := !size + size_type b;
-       hmk_const(s,b)
+    | Const(s,b) -> hmk_const(s,b)
     | Comb(u,v) -> hmk_comb(subst i su u, subst i su v)
     | Abs(u,v) ->
        match u with
-       | Var(_,b) -> size := !size + size_type b;
+       | Var(_,b) ->
           let s =
             if i < Array.length sy then sy.(i)
             else (log "y_max = %d\n%!" i; "y" ^ string_of_int i)
@@ -639,7 +635,6 @@ let canonical_term
        | _ -> assert false
   in
   fun t ->
-  size := 0;
   let tvs = type_vars_in_term t and vs = frees t in
   (* Type substitution mapping type variables of [t] to the canonical
      type variables [a0, a1, ...]. *)
@@ -649,7 +644,7 @@ let canonical_term
   (* Term substitution mapping term variables of [t] to the canonical
      term variables [x0, x1, ...]. *)
   and su' = List.mapi term_var vs' in
-  tvs, vs, bs, subst 0 su' t', !size
+  tvs, vs, bs, subst 0 su' t'
 ;;
 
 (****************************************************************************)
