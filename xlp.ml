77e08ae4e8229bb957d807c6ca11c20566b00041
Iván Martínez Comas <122469441+navimath@users.noreply.github.com>
2026-06-29 18:20:27 +0200

    Fix to make type Set in type parameters explicit in term abbreviations (#204)

--- xlp.ml
+++ xlp.ml
@@ -375,9 +375,14 @@ let print_let oc (t,t',_,_) =
 
 let decl_term_abbrev oc t (k,ntvs,bs) =
   let n = "term"^string_of_int k in
-  if ntvs > 0 then raw_update_tvs_map n ntvs;
   string oc "symbol "; string oc n;
-  for i=0 to ntvs-1 do string oc " a"; int oc i done;
+  if ntvs > 0 then
+    begin
+      raw_update_tvs_map n ntvs; 
+      string oc " (a0";
+      for i=1 to ntvs-1 do string oc " a"; int oc i done;
+      string oc " : Set)"
+    end;
   let decl_var i b =
     string oc " (x"; int oc i; string oc ": El "; abbrev_typ oc b; char oc ')'
   in
